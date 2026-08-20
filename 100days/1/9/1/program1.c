42d5f14c80109ea6060de9f9f6abd1d262de079c
abdulsamad73496-hue <abdulsamad73496@gmail.com>
2026-08-20 06:56:21 +0000

    Add files via upload

---
+++ 100days/1/9/1/program1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  int a,b,sum ;
+  printf("enter  two number:");
+scanf("%d",&a);
+scanf("%d",&b);
+sum=a+b;
+ printf("number are:%d %d\n",a,b);
+printf("sum=%d",sum);
+return 0 ;
+}
