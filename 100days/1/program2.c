fc9412b1d441cfcd64544ab0aadb465e723982b4
abdulsamad73496-hue <abdulsamad73496@gmail.com>
2026-08-31 17:13:33 +0000

    Add files via upload

--- 100days/1/program2.c
+++ 100days/1/program2.c
@@ -1,16 +1,24 @@
-//program to input  two number  and display  their sum ,differnce ,product and quotient
+//program to check no is perfect or not..
 #include<stdio.h>
 int main()
-{ 
-int  a,b,sum,diffrence,product,quotient;
-scanf("%d",&a);
-printf("frist number:%d",a);
-scanf("%d",&b);
-printf("second number:%d",b);
-sum=a+b;
-diffrence=a-b;
-product=a*b;
-quotient=a/b;
-printf("sum=%d/n,diffrence=%d/n,product=%d/n,quotient=%d/n",sum,diffrence,product,quotient);
+{
+int n,i=1,sum=0;
+printf("enter n");
+scanf("%d", &n);
+for ( i;(i<n);i++)
+   {
+    if (n%i==0)
+       {
+        sum=sum+i;
+        }
+    }
+if (sum==n)
+   {
+    printf(" perfect no");
+    }
+else 
+   {
+    printf("imperfect no");
+    }
 return 0;
 }
