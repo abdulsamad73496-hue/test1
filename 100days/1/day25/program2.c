2895fe838d21122964704a10f5f9f22cd878afa1
abdulsamad73496-hue <abdulsamad73496@gmail.com>
2026-09-04 17:24:05 +0000

    Add files via upload

---
+++ 100days/1/day25/program2.c
@@ -0,0 +1,15 @@
+//pattern
+#include<stdio.h>
+int main()
+{
+int i,j;
+    for (i=1;(i<=5);i++)
+         {
+          for(j=5;(j>=i);j--)
+             {printf("*");}
+          printf("\n");
+          for (j=0;(j<=(i-1));j++)
+                {printf(" ");}
+          }
+return 0;
+}
