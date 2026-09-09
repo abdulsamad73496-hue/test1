9d5c9e48cd55b3f0515c92b46b24c79e1521f231
abdulsamad73496-hue <abdulsamad73496@gmail.com>
2026-09-09 15:33:20 +0000

    Add files via upload

--- 100days/1/program1.c
+++ 100days/1/program1.c
@@ -1,14 +1,25 @@
-#include <stdio.h>
-
-int main()
+//search for an element in an array using  linear search..
+#include<stdio.h>
+int main ()
 {
-    int a, b, sum;
-
-    scanf("%d %d", &a, &b);
-
-    sum = a + b;
-
-    printf("Sum = %d", sum);
-
-    return 0;
+int a[100],n,b,temp;
+printf("How many element you have to input:");
+scanf("%d", &n);
+for (int i=1;i<n;i++)
+   {
+   printf("enter no:");
+   scanf("%d", &a[i]);
+   }
+printf("\n enter  which element you have to search?");
+scanf("%d", &b);
+for (int i=1;i<n;i++)
+    {
+    if (a[i]==b)
+      {temp=a[i];}
+     }
+if (temp==b)
+   {printf("element exist");}
+else 
+   {printf("element not exist");}
+return 0;
 }
