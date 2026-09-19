54cbd3e7088a01c3b53ceca45e4c1089ea527798
abdulsamad73496-hue <abdulsamad73496@gmail.com>
2026-09-19 19:42:42 +0000

    Add files via upload

--- 100days/1/day40/program1.c
+++ 100days/1/day40/program1.c
@@ -1,12 +1,27 @@
+//perform dignoal   traversal of a matrix..
 #include<stdio.h>
-int main()
+int main ()
 {
-int n;
-scanf("%d", &n);
-if (n=0)
- {printf("abdul");}
-else
- {printf("samad");
-  }
-return 0 ;
+int i,j;
+printf("Enter row and colum in array:");
+scanf("%d%d", &i,&j);
+int  a[i][j];
+for (int k=0;k<i;k++)
+   {for (int l=0;l<j;l++)
+       {scanf("%d", &a[k][l]);}
+   }    
+for(int k=0;k<i;k++)
+   {
+     for (int l=0;l<=k;l++)
+       {if (l==k)
+         {printf("%d",a[k][l]);}
+       else if (k>l && (k+l)%2==1)
+         {printf("%d%d", a[l][k],a[k][l]);}
+       else if (k<l &&  (k+l)%2==0)
+           {printf("%d%d", a[l][k],a[k][l]);} 
+      }
+   }
+return 0;
 }
+
+
